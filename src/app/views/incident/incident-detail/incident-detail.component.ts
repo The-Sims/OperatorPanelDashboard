9482192d36b127d4272e169a3060aa7385b017bc
bramk <bramkempen@gmail.com>
2018-12-10 21:01:12 +0100

    add connection to backend to confirm tips

--- src/app/views/incident/incident-detail/incident-detail.component.ts
+++ src/app/views/incident/incident-detail/incident-detail.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {Incident} from '../../../classes/incident';
 import {IncidentsService} from '../../../services/incidents.service';
 import {HttpErrorResponse} from '@angular/common/http';
+import {IncidentTip} from '../../../classes/incidentTip';
 
 @Component({
   selector: 'app-incident-detail',
@@ -18,6 +19,28 @@ export class IncidentDetailComponent implements OnInit {
   constructor(private incidentService: IncidentsService, private route: ActivatedRoute) {
   }
 
+  confirmTip(tip: IncidentTip) {
+    this.incidentService.confirmTip(tip.id).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+  confirm(incident: Incident) {
+    this.incidentService.confirm(incident.id).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+  close(incident: Incident) {
+    this.incidentService.close(incident.id).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+
   ngOnInit() {
     this.route.params.subscribe(
       params => {
@@ -25,10 +48,10 @@ export class IncidentDetailComponent implements OnInit {
         this.incidentService.getById(this.id).subscribe(
           data => {
             data.create_date = new Date(data.create_date);
-            for (let incidentDescription of data.incidentDescription){
+            for (let incidentDescription of data.incidentDescription) {
               incidentDescription.date = new Date(incidentDescription.date);
             }
-            for (let reinforcementInfo of data.reinforcementInfo){
+            for (let reinforcementInfo of data.reinforcementInfo) {
               reinforcementInfo.date = new Date(reinforcementInfo.date);
             }
             this.incident = Incident.fromJSON(data);

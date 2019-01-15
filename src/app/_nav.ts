5b67e98908e3ad0d7ee4b4696ed4ef9cd3f5066e
bramk <bramkempen@gmail.com>
2019-01-15 14:02:11 +0100

    remove html

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -3,10 +3,6 @@ export const navItems = [
         name: 'Dashboard',
         url: '/dashboard',
         icon: 'icon-speedometer',
-        badge: {
-            variant: 'info',
-            text: 'NEW'
-        }
     },
     {
         title: true,
@@ -55,84 +51,6 @@ export const navItems = [
                 icon: 'icon-drop'
             },
         ],
-    },
-    {
-        title: true,
-        name: 'Components'
-    },
-    {
-        name: 'Base',
-        url: '/base',
-        icon: 'icon-puzzle',
-        children: [
-            {
-                name: 'Cards',
-                url: '/base/cards',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Carousels',
-                url: '/base/carousels',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Collapses',
-                url: '/base/collapses',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Forms',
-                url: '/base/forms',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Pagination',
-                url: '/base/paginations',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Popovers',
-                url: '/base/popovers',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Progress',
-                url: '/base/progress',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Switches',
-                url: '/base/switches',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Tables',
-                url: '/base/tables',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Tabs',
-                url: '/base/tabs',
-                icon: 'icon-puzzle'
-            },
-            {
-                name: 'Tooltips',
-                url: '/base/tooltips',
-                icon: 'icon-puzzle'
-            }
-        ]
-    },
-    {
-        name: 'Charts',
-        url: '/charts',
-        icon: 'icon-pie-chart'
-    },
-    {
-        divider: true
-    },
-    {
-        title: true,
-        name: 'Extras',
     },
     {
         name: 'Pages',
